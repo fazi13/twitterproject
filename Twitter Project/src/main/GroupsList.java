5402d7cbe86406e54199e7476d80ad39169a3d21
Dylan <Dylan@DESKTOP-3TQVN2F>
2017-07-13 06:34:31 -0700

    fixed group exists

--- Twitter Project/src/main/GroupsList.java
+++ Twitter Project/src/main/GroupsList.java
@@ -36,37 +36,11 @@ public class GroupsList {
 	}
 	
 	public static boolean groupExists(int id){
-		if(id == 0){
-			return true;
-		}
-		//binary search group list for that id
-		Collections.sort(allGroups);
-		//-1 means not found return not exists
-		if(binarySearch(allGroups, 0, allGroups.size(), id) == -1){
-			return false;
-		}
-		return true;
-	}
-	
-	private static int binarySearch(ArrayList<Group> list, int l, int r, int x){
-		if(r >= list.size()){
-			//if r > array size then does not exist
-			return -1;
-		}
-		if(r > 1){
-			int mid = l + (r-l)/2;
-			if(list.get(mid).getGroupID() == x){
-				return mid;
-			}
-			if(list.get(mid).getGroupID() > x){
-				return binarySearch(list, l, mid - 1, x);
-			}
-			return binarySearch(list, mid + 1, r, x);
-		}else if(r == 1){
-			if(list.get(0).getGroupID() == x){
-				return 0;
+		for(int i = 0; i < allGroups.size(); i++){
+			if(allGroups.get(i).getGroupID() == id){
+				return true;
 			}
 		}
-		return -1;
+		return false;
 	}
 }
